03bdbb63a60cee8a3b9b955e9ba4770b52104ca0
Carnagion <indraneel.mahendrakumar@gmail.com>
2025-04-29 18:32:50 +0100

    Fix typo

--- init/create-birthdays.sql
+++ init/create-birthdays.sql
@@ -2,5 +2,5 @@ create table if not exists birthdays (
     user_id integer not null,
     guild_id integer not null,
     birthday text not null,
-    unique(user_Id, guild_id)
+    unique(user_id, guild_id)
 );
